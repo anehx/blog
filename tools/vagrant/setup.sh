94e7d0f10fb2ae57320162341fdea5ea25fe854d
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2015-12-11 09:14:23 +0100

    Improved dev env

--- tools/vagrant/setup.sh
+++ tools/vagrant/setup.sh
@@ -6,14 +6,16 @@ apt-get -q -y install apache2 php5 php5-dev php5-xdebug curl vim php5-sqlite mak
 
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 apt-get install -y nodejs
+npm i -g bower
 
 a2enmod rewrite
+a2enmod proxy
+a2enmod proxy_http
 
 usermod -a -G vagrant www-data
 
-rm /etc/apache2/sites-enabled/000-default
-ln -s /etc/apache2/sites-enabled/blog /vagrant/tools/vagrant/vhost.conf
+rm /etc/apache2/sites-enabled/*
+ln -s /vagrant/tools/vagrant/vhost.conf /etc/apache2/sites-enabled/blog.conf
 
 service apache2 restart
-npm i -g bower
 cd /vagrant && make install
