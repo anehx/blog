bbbe00b93ca9c0b8f7bf74cebbd4e747cf0155a5
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2015-12-12 16:48:00 +0100

    Improved index page

--- frontend/app/index/route.js
+++ frontend/app/index/route.js
@@ -1,10 +1,14 @@
 App.router.route('', function() {
   App.store.getList('blogs', null, function(data) {
-    var content = ''
+    var content = '<div class="container"><h1 class="page-title">Blogs</h1><div class="blog-list">'
     data.data.forEach(function(blog) {
-      content += `<a href="/${blog.id}" class="history-link">${blog.name}</a><br>`
+      content += `
+        <div>
+          <a href="/${blog.id}" class="history-link link-postfix">${blog.name}</a><br>
+        </div>
+      `
     })
 
-    App.router.setContent(content)
+    App.router.setContent(content + '</div></div>')
   })
 })
